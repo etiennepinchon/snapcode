aba6e7edf535c5731ebc157f75ba63f20d022403
Marek Hrusovsky <xhruso00@gmail.com>
2016-09-16 19:44:57 +0200

    Initial Commit

---
+++ snapcode/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  snapcode
+//
+//  Created by Marek Hrusovsky on 9/16/16.
+//  Copyright © 2016 Marek Hrusovsky. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
